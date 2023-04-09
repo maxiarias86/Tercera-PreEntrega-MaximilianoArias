9c9d978e8c11d819481e34121154d7321fa80d81
maxiarias <maxi@MacBook-Air-de-Maxi.local>
2023-04-09 10:48:00 -0300

    Primer usuario, medico, paciente y eco cargada

--- AppEco/models.py
+++ AppEco/models.py
@@ -15,15 +15,18 @@ class Paciente(models.Model):
     mail=models.EmailField()
     fecha_nacimiento=models.DateField()
     sexo=models.CharField(max_length=2, choices=sexos, default='F')
-    #Agregar sexo
+    
     def __str__(self):
-        return f'Paciente {Paciente.nombre} {Paciente.apellido}, DNI {Paciente.DNI}'
+        return f'Paciente {self.nombre} {self.apellido}, DNI {self.DNI}'
 
 class Medico(models.Model):
     MN= models.IntegerField()
     nombre=models.CharField(max_length=40)
     apellido=models.CharField(max_length=40)
     mail=models.EmailField()
+
+    def __str__(self):
+        return f'Dr. {self.apellido}, {self.nombre}, MN: {self.MN}'
 
 class Ecografia(models.Model):
     fecha_estudio=models.DateField()
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # markdown
+
+superuser
+admin
+maxiarias@gmail.com
+Arraial2023!
+
+rene.favaloro
+bypassAoC
