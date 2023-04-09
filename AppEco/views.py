d5b2de33e96a32430f278e4c123ebdbccce9781d
maxiarias <maxi@MacBook-Air-de-Maxi.local>
2023-04-09 09:47:04 -0300

    Primer prueba correcta con herencia de HTML

--- AppEco/views.py
+++ AppEco/views.py
@@ -5,10 +5,10 @@ from AppEco.models import Paciente, Medico, Ecografia
 # Create your views here.
 
 def inicio(request):
-    return render (request, 'inicio.html')
+    return render (request, 'AppEco/inicio.html')
 def pacientes(request):
-    return render (request, 'pacientes.html')
+    return render (request, 'AppEco/pacientes.html')
 def medicos(request):
-    return render (request, 'medicos.html')
+    return render (request, 'AppEco/medicos.html')
 def ecografias(request):
-    return render (request, 'ecografias.html')
+    return render (request, 'AppEco/ecografias.html')
