87c42279f50b5846028abaebd72ffb965a8e794c
maxiarias <maxi@MacBook-Air-de-Maxi.local>
2023-04-12 09:57:16 -0300

    Antes de hacer los cambios de nuevos formularios

--- AppEco/forms.py
+++ AppEco/forms.py
@@ -14,3 +14,13 @@ class PacienteFormulario(forms.Form):
     mail=forms.EmailField()
     fecha_nacimiento=forms.DateField()
     sexo=forms.ChoiceField(choices=generos, required=True, label='Seleccione su genero')
+
+class FormNuevaEcografia(forms.Form):
+    fecha_estudio=forms.DateField()
+    medico=forms.ForeignKey(Medico, null=True, blank=True, on_delete=models.SET_NULL)
+    paciente=forms.ForeignKey(Paciente, on_delete=models.CASCADE)
+    DBP=forms.FloatField()
+    CC=forms.FloatField()
+    CA=forms.FloatField()
+    LF=forms.FloatField()
+    PFE=forms.IntegerField()    
